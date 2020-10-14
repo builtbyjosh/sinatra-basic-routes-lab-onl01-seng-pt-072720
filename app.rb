7eef96086e60264cf672136257ea10582db03cb2
builtbyjosh <admin@builtbyjosh.com>
2020-10-13 21:15:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Joshua"
+    end
+
+    get '/hometown' do
+        "My hometown is Holland, MI"
+    end
+
+    get '/favorite-song' do
+        'My favorite song is Flow, by Shawn James'
+    end
 end
